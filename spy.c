3b62f7ba35f3ad0e93f9c583f236ffd5898ee3f1
RagnvaldArnulfson <gus683@hotmail.fr>
2016-02-21 00:52:45 +0100

    Create spy.c

---
+++ spy.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float a = 1., b = 1.;
+
+    while(!((a+b)-a-b))
+        a*=2.;
+    while((a+b)-a-b)
+        b+=1.;
+
+    printf("a=%f\nb=%f\n",a,b);
+
+    system("pause");
+    return 0;
+}
