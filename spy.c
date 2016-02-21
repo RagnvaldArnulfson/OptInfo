77d0fa5d97f9cd5dffb8a042eea5304cbf4ab02e
RagnvaldArnulfson <gus683@hotmail.fr>
2016-02-21 16:30:34 +0100

    Update spy.c

--- spy.c
+++ spy.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 int main(){
-    float a = 1., b = 1.;
+    float a = 1, b = 1;
 
     while(!((a+b)-a-b))
-        a*=2.;
+        a*=2;
     while((a+b)-a-b)
-        b+=1.;
+        b+=1;
 
     printf("a=%f\nb=%f\n",a,b);
 
