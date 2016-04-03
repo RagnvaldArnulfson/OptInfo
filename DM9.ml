8894f3b4057eee4c310164690a19ae5065c45c49
RagnvaldArnulfson <gus683@hotmail.fr>
2016-04-03 22:53:54 +0200

    Update DM9.ml

--- DM9.ml
+++ DM9.ml
@@ -98,3 +98,78 @@ let decomposition n =
 	in imprime "" (regroupe [] 1 (decomp [] n));;
 
 decomposition 7521;;
+
+let rec intervalle = fun
+	| 0 0 l -> [] 
+	| 0 b (h::q) -> h::(intervalle 0 (b-1) q)
+	| a b (h::q) -> intervalle (a-1) b q
+	| a b [] -> [];;
+
+let operation c d =
+	let (a,a') = c and (b,b') = d in
+	if b - a > 0
+		then
+		begin
+			if b mod a = 0 then
+				(b - a, "(" ^ b' ^ " - " ^ a' ^ ")")::(b / a, "(" ^ b' ^ " / " ^ a' ^ ")")::[a + b, "(" ^ a' ^ " + " ^ b' ^ ")"; a * b, "(" ^ a' ^ " * " ^ b' ^ ")"]
+			else
+				(b - a, "(" ^ b' ^ " - " ^ a' ^ ")")::[a + b, "(" ^ a' ^ " + " ^ b' ^ ")"; a * b, "(" ^ a' ^ " * " ^ b' ^ ")"]
+		end
+	else if a <> b
+		then
+		begin
+			if a mod b = 0 then
+				(a - b, "(" ^ a' ^ " - " ^ b' ^ ")")::(a / b, "(" ^ a' ^ " / " ^ b' ^ ")")::[a + b, "(" ^ a' ^ " + " ^ b'  ^ ")"; a * b, "(" ^ a' ^ " * " ^ b' ^ ")"]
+			else
+				(a - b, "(" ^ a' ^ " - " ^ b' ^ ")")::[a + b, "(" ^ a' ^ " + " ^ b'  ^ ")"; a * b, "(" ^ a' ^ " * " ^ b' ^ ")"]
+		end
+	else
+		(a / b, "(" ^ a' ^ " / " ^ b' ^ ")")::[a + b, "(" ^ a' ^ " + " ^ b' ^ ")"; a * b, "(" ^ a' ^ " * " ^ b' ^ ")"];;
+
+let rec elem_comp_liste t = function
+	| [] -> []
+	| h::q -> (operation t h)@(elem_comp_liste t q);;
+
+let rec liste_comp_liste a = function
+	| [] -> []
+	| h::q -> (elem_comp_liste h a)@(liste_comp_liste a q);;
+
+let rec decoupe = function
+  | [] -> []
+  | [a] -> []
+  | [a; b] -> [[a], [b]]
+  | (h::q) -> ([h], q)::(map (function x, y -> (x, y)) (decoupe q)) ;;	
+	
+let evaluation plaquettes =
+	let rec eval = function
+		| [] -> []
+		| (a,b)::q -> liste_comp_liste a b@eval q
+	in eval (decoupe plaquettes);;
+
+let etape = function
+	| [] -> []
+	| (a,b)::q -> (elem_comp_liste a (map (function x, y -> x) q))@q;;
+
+let etape = function
+	| [] -> []
+	| a::q -> elem_comp_liste a q@q;;
+
+let rec find_n n = function
+	| [] -> ""
+	| (a,b)::q when a = n -> b
+	| (a,b)::q -> find_n n q;;
+
+(*let evaluation plaquettes =
+	let rec eval = function
+		| [] -> []
+		| (a,b)::q -> etape q plaquettes
+	in eval (decoupe plaquettes);;*)
+
+let lecompteestbon n p =
+	let rec comptebon n plaquettes =
+		let plaq = evaluation plaquettes in
+			let ans = find_n n plaq in
+				if ans = "" then (comptebon n plaq) else ans
+	in comptebon n (map (function x -> x, string_of_int(x)) p);;
+
+lecompteestbon 952 [25;50;75;100;3;6];;
